3f6859dc2f0a7227355e66a445076bd6e70d80e6
Rishbh Pandram <rishabh@onehash.ai>
2023-07-08 18:00:27 +0530

    sending emails by site status

--- bettersaas/api.py
+++ bettersaas/api.py
@@ -1,5 +1,7 @@
 import frappe
-
+from datetime import datetime,timedelta
+from frappe.utils import add_to_date,getdate
+import subprocess
 
 @frappe.whitelist()
 def get_bench_details_for_cloudwatch():
@@ -12,3 +14,51 @@ def get_bench_details_for_cloudwatch():
     """
     details = {}
     number_of_total_sites = frappe.db.count("SaaS sites")
+@frappe.whitelist(allow_guest=True)
+def delete_sites():
+    sites = frappe.db.get_all('SaaS sites', fields=['*'], filters={
+        'last_login': ['>', datetime.now()-timedelta(days=28)]
+    })
+    present_date = getdate()
+    for site in sites:
+        email = site.linked_email
+        user_name = site.name
+        if site.plan == 'free':
+            warning_mail_start_date = add_to_date(site.expiry_date)-timedelta(days=4)
+            warning_mail_end_date = add_to_date(site.expiry_date)-timedelta(days=1)
+            if warning_mail_start_date <= present_date <= warning_mail_end_date:
+                content = 'This is to inform you that your OneHash account with the email address {e_address} will be permanently deleted on {exp_date}. You will no longer be able to access your account or recover any data'.format(e_address=email,exp_date = site.expiry_date)
+                return send_email(email, content, user_name)
+
+            elif present_date == add_to_date(site.expiry_date):
+                subprocess.call('bench drop-site {s_name} --db-root-password {db_pass}'.format(s_name=site.site_name, db_pass='ris@123'), shell=True)
+                content = 'We regret to inform you that your OneHash account with the email address {e_address} has be permanently deleted. Unfortunately, you will no longer be able to access your account or recover any data'.format(e_address=email)
+                return send_email(email, content, user_name)
+
+        elif site.plan == 'purchased':
+            warning_mail_start_date = add_to_date(site.expiry_date) - timedelta(days=8)
+            warning_mail_end_date = add_to_date(site.expiry_date) - timedelta(days=1)
+            if warning_mail_start_date <= present_date <= warning_mail_end_date:
+                content = 'This is to inform you that your OneHash account with the email address {e_address} will expire on {exp_date}. You will no longer be able to access your account'.format(e_address=email,exp_date = site.expiry_date)
+                return send_email(email, content, user_name)
+            
+            elif present_date == add_to_date(site.expiry_date):
+                content = 'This is to inform you that your OneHash account with the email address {e_address} has expired. You will no longer be able to access your account'.format(e_address=email,exp_date = site.expiry_date)
+                return send_email(email, content, user_name)
+
+
+def send_email(email, content, user_name):
+    template = 'account_status_email'
+    subject = 'Account Status'
+    args = {
+        "title": subject,
+        "f_name": user_name,
+        "content": content
+    }
+    frappe.sendmail(
+        recipients=email,
+        template=template,
+        subject=subject,
+        args=args,
+    )
+    return 1
