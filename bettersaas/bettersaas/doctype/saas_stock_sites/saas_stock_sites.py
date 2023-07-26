67c05573838b48ed18c437075347d3ae19f4035d
abhishek-chougule <chouguleabhis@gmail.com>
2023-07-26 12:07:31 +0530

    sda

--- bettersaas/bettersaas/doctype/saas_stock_sites/saas_stock_sites.py
+++ bettersaas/bettersaas/doctype/saas_stock_sites/saas_stock_sites.py
@@ -54,67 +54,69 @@ def deleteUsedSites():
 
 @frappe.whitelist()
 def refreshStockSites(*args, **kwargs):
-    # this function runs every day and maintains the stock site
-    print("refreshing stock sites")
-    config = getSiteConfig()
-    commands = []
-    currentStock = frappe.db.get_list("SaaS stock sites", filters={"is_used": "no"})
-    print("In stock", len(currentStock))
-    db_values = []
-    if len(currentStock) < int(config.stock_site_count):
-        number_of_sites_to_stock = int(config.stock_site_count) - len(currentStock)
-        for _ in range(number_of_sites_to_stock):
-            import string
-            import random
-            letters = string.ascii_lowercase
-            random_string_util = "".join(random.choice(letters) for i in range(10))
-            subdomain =random_string_util
-            adminPassword = random_string(5)
-            this_command = []
-            this_command.append(
-                "bench new-site {} --install-app erpnext  --admin-password {} --db-root-password {}".format(
-                    subdomain + "." + domain, adminPassword, config.db_password
-                )
-            )
-            apps_to_install = [
-                frappe.get_doc("Available Apps", x.as_dict().app).app_name
-                for x in frappe.get_doc("SaaS settings").apps_to_install
-            ]
-            print(apps_to_install)
-            for app in apps_to_install:
+    conf=frappe.get_doc('SaaS settings')
+    if conf.ssc_enabled==1:
+        # this function runs every day and maintains the stock site
+        print("refreshing stock sites")
+        config = getSiteConfig()
+        commands = []
+        currentStock = frappe.db.get_list("SaaS stock sites", filters={"is_used": "no"})
+        print("In stock", len(currentStock))
+        db_values = []
+        if len(currentStock) < int(config.stock_site_count):
+            number_of_sites_to_stock = int(config.stock_site_count) - len(currentStock)
+            for _ in range(number_of_sites_to_stock):
+                import string
+                import random
+                letters = string.ascii_lowercase
+                random_string_util = "".join(random.choice(letters) for i in range(10))
+                subdomain =random_string_util
+                adminPassword = random_string(5)
+                this_command = []
                 this_command.append(
-                    "bench --site {} install-app {}".format(
-                        subdomain + "." + domain, app.strip()
+                    "bench new-site {} --install-app erpnext  --admin-password {} --db-root-password {}".format(
+                        subdomain + "." + domain, adminPassword, config.db_password
                     )
                 )
+                apps_to_install = [
+                    frappe.get_doc("Available Apps", x.as_dict().app).app_name
+                    for x in frappe.get_doc("SaaS settings").apps_to_install
+                ]
+                print(apps_to_install)
+                for app in apps_to_install:
+                    this_command.append(
+                        "bench --site {} install-app {}".format(
+                            subdomain + "." + domain, app.strip()
+                        )
+                    )
 
-            this_command.append(
-                "bench --site {} install-app clientside".format(
-                    subdomain + "." + domain
+                this_command.append(
+                    "bench --site {} install-app clientside".format(
+                        subdomain + "." + domain
+                    )
                 )
-            )
-            adminSubdomain = frappe.conf.get("admin_subdomain")
-            this_command.append(
-                "bench --site {} execute bettersaas.bettersaas.doctype.saas_stock_sites.saas_stock_sites.insertSite --args \"'{}','{}'\"".format(
-                    adminSubdomain + "." + domain, subdomain, adminPassword
+                adminSubdomain = frappe.conf.get("admin_subdomain")
+                this_command.append(
+                    "bench --site {} execute bettersaas.bettersaas.doctype.saas_stock_sites.saas_stock_sites.insertSite --args \"'{}','{}'\"".format(
+                        adminSubdomain + "." + domain, subdomain, adminPassword
+                    )
                 )
-            )
-            site_defaults = frappe.get_doc("SaaS settings")
-            this_command.append(
-                "bench --site {} set-config max_users {}".format(
-                    subdomain + "." + domain, site_defaults.default_user_limit
+                site_defaults = frappe.get_doc("SaaS settings")
+                this_command.append(
+                    "bench --site {} set-config max_users {}".format(
+                        subdomain + "." + domain, site_defaults.default_user_limit
+                    )
                 )
-            )
-            this_command.append(
-                "bench --site {} set-config max_email {}".format(
-                    subdomain + "." + domain, site_defaults.default_email_limit
+                this_command.append(
+                    "bench --site {} set-config max_email {}".format(
+                        subdomain + "." + domain, site_defaults.default_email_limit
+                    )
                 )
-            )
-            this_command = " ; ".join(this_command)
-            print("adding to queue,", this_command)
-            method = "bettersaas.bettersaas.doctype.saas_stock_sites.saas_stock_sites.create_multiple_sites_in_parallel"
-            db_values.append([subdomain, adminPassword])
-            frappe.enqueue(method, command=this_command, db_values=db_values,queue="short")
+                this_command = " ; ".join(this_command)
+                print("adding to queue,", this_command)
+                method = "bettersaas.bettersaas.doctype.saas_stock_sites.saas_stock_sites.create_multiple_sites_in_parallel"
+                db_values.append([subdomain, adminPassword])
+                frappe.enqueue(method, command=this_command, db_values=db_values,queue="short")
     
     return "Database will be updated soon with stock sites "
 
