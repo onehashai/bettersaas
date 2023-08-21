9846ef6e668b2fd1b30d57d39426e53b2d02f84b
Abhishek Chougule <chouguleabhis@gmail.com>
2023-08-21 12:40:51 +0530

    Fix

--- bettersaas/bettersaas/doctype/saas_users/saas_users.py
+++ bettersaas/bettersaas/doctype/saas_users/saas_users.py
@@ -244,9 +244,9 @@ def create_lead(saas_user):
 		lead_doc.email_id = saas_user.email
 		lead_doc.mobile_no = saas_user.phone
 		lead_doc.primary_mobile = saas_user.phone
-		lead.lead_name = saas_user.first_name+" "+saas_user.last_name
-		lead.first_name = saas_user.first_naame
-		lead.linked_saas_site = saas_user.site
+		lead_doc.lead_name = saas_user.first_name+" "+saas_user.last_name
+		lead_doc.first_name = saas_user.first_naame
+		lead_doc.linked_saas_site = saas_user.site
 		lead_doc.flags.ignore_permissions = True
 		lead_doc.save(ignore_permissions=True)
 		
