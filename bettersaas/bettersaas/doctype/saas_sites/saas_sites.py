db70f8e37d3517705b3f8d509109305f82da5438
Abhishek Chougule <chouguleabhis@gmail.com>
2023-09-12 17:20:43 +0530

    Update saas_sites.py

--- bettersaas/bettersaas/doctype/saas_sites/saas_sites.py
+++ bettersaas/bettersaas/doctype/saas_sites/saas_sites.py
@@ -27,7 +27,7 @@ def delete_thesite(site_name):
 @frappe.whitelist()
 def restore_thesite(site_name):
     commands = []
-    commands.append("mv archived/{site} sites/".format(site=site_name))
+    commands.append("mv archived/sites/{site} sites/".format(site=site_name))
     commands.append("bench --site {site} restore sites/{site}/private/backups/onehash_backup.sql.gz".format(site=site_name))
     executeCommands(commands)
     frappe.msgprint('Site Restored !')
